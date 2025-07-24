06e5b19bc81c5b0ec168e3774fd8a91425142086
HenriqueLopes-dev <ohenriquelopes.dev@gmail.com>
2025-07-24 16:28:04 -0300

    mudanca no botao novamente, agora funciona

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -13,6 +13,7 @@ import { FaLinux } from "react-icons/fa";
 import { Techs } from "./Techs";
 
 export const MainContent = () => {
+  const github = "https://github.com/HenriqueLopes-dev";
   return (
     <main className="flex-1 p-8 min-h-screen bg-gradient-to-br from-gray-700 via-gray-800  to-gray-900">
       <h1 className="text-4xl font-bold">Henrique Lopes</h1>
@@ -23,14 +24,16 @@ export const MainContent = () => {
         atualmente estou no sexto período...
       </p>
 
-      <button
+      <a
         className="flex items-center bg-gradient-to-r from-green-500 to-emerald-600 hover:from-green-400
         hover:to-emerald-500 text-white font-semibold py-2 px-6 rounded-full shadow-xl
-        hover:shadow-2xl transform hover:scale-105 transition-all duration-300 ease-in-out w-auto"
+        hover:shadow-2xl transform hover:scale-105 transition-all duration-300 ease-in-out w-43"
+        href={github}
+        target="_blank"
       >
         <SiGithub className="inline mr-2" />
         Ver Projetos
-      </button>
+      </a>
 
       <section className="mt-4">
         <h2 className="text-2xl font-bold mb-4">Meus conhecimentos:</h2>
